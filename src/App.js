9245d4c0be5a9013f50f442954129f7001ace12a
Gerard <gerard.eklu@yahoo.fr>
2018-03-09 03:03:49 -0500

    Adding robotFriends

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, {Component} from "react";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import Scroll from "./scroll";
+
+
+
+class App extends Component {
+
+    constructor() {
+
+        super()
+        this.state = {
+            
+            robots: [],
+            searchfield: " "
+         }
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response=> response.json())
+            .then(users => this.setState({ robots: users}));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+       
+    }
+     render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+
+        })
+
+        return (
+            <div className="tc">
+                <h1>RobotFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                 <CardList robots={filteredRobots} />
+                </Scroll>
+            </div>
+        );
+
+       }
+
+}
+
+export default App;
